58734c614e1700b72a92016c68e14917acdc4983
Raylex Lee <raylexym57lee@gmail.com>
2022-04-21 20:59:11 +0800

    Prepare for generating htmls

---
+++ utility/copy2githubio.sh
@@ -0,0 +1,7 @@
+~/scp2githubio 明朝那些事兒1.html
+~/scp2githubio 明朝那些事兒2.html
+~/scp2githubio 明朝那些事兒3.html
+~/scp2githubio 明朝那些事兒4.html
+~/scp2githubio 明朝那些事兒5.html
+~/scp2githubio 明朝那些事兒6.html
+~/scp2githubio 明朝那些事兒7大結局.html
---
+++ utility/make_new_htmls.sh
@@ -0,0 +1,7 @@
+./make_zhtextapp_html.txt 明朝那些事兒1
+./make_zhtextapp_html.txt 明朝那些事兒2
+./make_zhtextapp_html.txt 明朝那些事兒3
+./make_zhtextapp_html.txt 明朝那些事兒4
+./make_zhtextapp_html.txt 明朝那些事兒5
+./make_zhtextapp_html.txt 明朝那些事兒6
+./make_zhtextapp_html.txt 明朝那些事兒7大結局
